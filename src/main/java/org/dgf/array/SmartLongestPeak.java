8f482c62d4959f999ea59c02bc53979d311fb506
Elvis.Du <jackdgf56@gmail.com>
2021-03-31 15:01:41 +0800

    longest peak2

--- src/main/java/org/dgf/array/SmartLongestPeak.java
+++ src/main/java/org/dgf/array/SmartLongestPeak.java
@@ -52,4 +52,28 @@ public class SmartLongestPeak {
 
         return maxLen;
     }
+
+    public static int longestPeak2(int[] array){
+        if (array.length < 3) return 0;
+        int maxPeakLen = 0;
+        int i = 1;
+        while (i < array.length -1) {
+            boolean isPeak = array[i-1] < array[i] && array[i] > array[i+1];
+            if(!isPeak) {
+                i+= 1;
+                continue;
+            }
+            int leftIdx = i-2;
+            while (leftIdx >= 0 && array[leftIdx] < array[leftIdx+1]) {
+                leftIdx--;
+            }// leftIdx+1
+            int rightIdx = i+2;
+            while(rightIdx < array.length && array[rightIdx] < array[rightIdx-1]) {
+                rightIdx++;
+            }
+            maxPeakLen = Math.max(maxPeakLen, rightIdx-1-leftIdx-1+1);
+            i = rightIdx;
+        }
+        return maxPeakLen;
+    }
 }
