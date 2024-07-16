f66cfbc23f40a84190701bac1a40941c8855025c
jackDu <jackdgf56@gmail.com>
2024-07-16 17:53:05 +0800

    Add Possible sub string approach 2

--- src/main/java/org/dgf/string/AllPossibleSubString.java
+++ src/main/java/org/dgf/string/AllPossibleSubString.java
@@ -1,7 +1,9 @@
 package org.dgf.string;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
  string input = "abc"
@@ -42,10 +44,36 @@ public class AllPossibleSubString {
             for (String s : subs) {
                 result.add(cur + s);
             }
-            result.stream().forEach(System.out::println);
-            System.out.println(i + " ---------- ");
         }
 
+        return result;
+    }
+
+    /*
+     " a  b  c "
+       0  0  1  -> c   (1)
+       0  1  0  -> b   (2)
+       1  0  0  -> a   (4)
+       0  1  1  -> bc  (3)
+       1  0  1  -> ac  (5)
+       1  1  0  -> ab  (6)
+       1  1  1  -> abc (7)
+
+     */
+    public static List<String> getPossibleStringWay2(String input) {
+        final List<String> result = new ArrayList<>();
+        final int maxNum = 1 << input.length() ;
+
+        for (int i = 1; i < maxNum; i++) {
+            String res = "";
+            for(int bit = 0 ; bit < input.length(); bit++) {
+                if ((i & (1 << bit)) != 0) {
+                    res = input.charAt(input.length() - 1 - bit) + res;
+                }
+            }
+            result.add(res);
+        }
+        Collections.sort(result);;
         return result;
     }
 
@@ -53,5 +81,14 @@ public class AllPossibleSubString {
         final var input = "abc";
         final var result = getPossibleString(input);
         result.stream().forEach(System.out::println);
+        System.out.println(" ---------- ");
+        final var input2 = "aa";
+        final var result2 = getPossibleString(input2);
+        result2.stream().forEach(System.out::println);
+        System.out.println(" ---------- ");
+        final var result3 = getPossibleStringWay2 (input);
+        result3.stream().forEach(System.out::println);
+        //final var join3 = result3.stream().collect(Collectors.joining(" ")).toString();
+        //System.out.println(join3);
     }
 }
