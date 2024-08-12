ac1727dee043c3e00959db0ce83de8d8df26e128
jackDu <jackdgf56@gmail.com>
2024-08-12 17:52:48 +0800

    add string rotate

---
+++ src/main/java/org/dgf/string/StringRotation.java
@@ -0,0 +1,10 @@
+package org.dgf.string;
+
+public class StringRotation {
+
+    public static void main(String[] argv){
+        String s1 = "aba";
+        String s2 = "baa";
+        System.out.println((s1+s1).contains(s2));
+    }
+}
